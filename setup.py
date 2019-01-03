5f2a70c8b677aa5ebb50c4ee9ed8d26a2297e038
John Smith <doom.gloom.tuturu@gmail.com>
2019-01-03 15:00:37 +0400

    bump version

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='searchbyimage',
-    version='0.0.1',
+    version='0.0.2',
     packages=find_packages(),
     install_requires=[
         'pycurl',
